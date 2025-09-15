1789285396b08365c85c2017ab8d34a80c3dd013
Hojeong016 <skflsehdn@naver.com>
2025-09-15 16:38:36 +0900

    키워드 상품 검색 - Naver 검색Api 연동 - 화면 코드스니펫 적용

---
+++ src/main/java/com/sparta/myselectshop/naver/service/NaverApiService.java
@@ -0,0 +1,71 @@
+package com.sparta.myselectshop.naver.service;
+
+import com.sparta.myselectshop.naver.dto.ItemDto;
+import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Service
+public class NaverApiService {
+
+    @Value("${api.client-id}")
+    private String clientId;
+
+    @Value("${api.client-secret}")
+    private String clientSecret;
+
+    private final RestTemplate restTemplate;
+
+    public NaverApiService(RestTemplateBuilder restTemplate) {
+        this.restTemplate = restTemplate.build();
+    }
+
+    public List<ItemDto> searchItems(String query) {
+        URI uri = UriComponentsBuilder
+                .fromUriString("https://openapi.naver.com")
+                .path("/v1/search/shop.json")
+                .queryParam("display", 15)
+                .queryParam("query", query)
+                .encode()
+                .build()
+                .toUri();
+        log.info("uri = " + uri);
+
+        RequestEntity<Void> requestEntity = RequestEntity
+                .get(uri)
+                .header("X-Naver-Client-Id", clientId)
+                .header("X-Naver-Client-Secret", clientSecret)
+                .build();
+
+        ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+
+        log.info("NAVER API Status Code : " + responseEntity.getStatusCode());
+
+        return fromJSONtoItems(responseEntity.getBody());
+    }
+
+    public List<ItemDto> fromJSONtoItems(String responseEntity) {
+        JSONObject jsonObject = new JSONObject(responseEntity);
+        JSONArray items  = jsonObject.getJSONArray("items");
+        List<ItemDto> itemDtoList = new ArrayList<>();
+
+        for (Object item : items) {
+            ItemDto itemDto = new ItemDto((JSONObject) item);
+            itemDtoList.add(itemDto);
+        }
+
+        return itemDtoList;
+    }
+}
