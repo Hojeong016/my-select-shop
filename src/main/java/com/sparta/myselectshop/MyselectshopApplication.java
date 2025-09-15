88517aabf95fddd7a8972b03d2f0d230fed1cf5f
Hojeong016 <skflsehdn@naver.com>
2025-09-15 16:11:12 +0900

    init commit

---
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -0,0 +1,14 @@
+package com.sparta.myselectshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class MyselectshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopApplication.class, args);
+    }
+
+}
