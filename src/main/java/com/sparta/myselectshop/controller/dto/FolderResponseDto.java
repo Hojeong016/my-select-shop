d17fc9532137a32c493bfef18a96747e022758c9
Hojeong016 <skflsehdn@naver.com>
2025-09-16 11:15:21 +0900

    관심 폴더 추가 구현

---
+++ src/main/java/com/sparta/myselectshop/controller/dto/FolderResponseDto.java
@@ -0,0 +1,15 @@
+package com.sparta.myselectshop.controller.dto;
+
+import com.sparta.myselectshop.entity.Folder;
+import lombok.Getter;
+
+@Getter
+public class FolderResponseDto {
+    private Long id;
+    private String name;
+
+    public FolderResponseDto(Folder folder) {
+        this.id = folder.getId();
+        this.name = folder.getName();
+    }
+}
